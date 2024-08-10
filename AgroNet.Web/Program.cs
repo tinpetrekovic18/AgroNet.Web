4f9e855794c4af9a7bd8cb6c163a66f04ea62517
Tin Petreković <tpetrekov@tvz.hr>
2024-08-11 00:14:07 +0200

    Facebook Login

--- AgroNet.Web/Program.cs
+++ AgroNet.Web/Program.cs
@@ -15,6 +15,12 @@ services.AddAuthentication().AddGoogle(googleOptions =>
     googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
 });
 
+services.AddAuthentication().AddFacebook(facebookOptions =>
+{
+    facebookOptions.AppId = configuration["Authentication:Facebook:AppId"];
+    facebookOptions.AppSecret = configuration["Authentication:Facebook:AppSecret"];
+});
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
